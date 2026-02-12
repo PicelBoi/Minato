7012bc7836d5561f185a254e9d4664597aab668d
PicelBoi <picelboi@picelboi.xyz>
2026-02-11 20:54:38 -0500

    *try* to add logging

--- Minato/Program.cs
+++ Minato/Program.cs
@@ -1,10 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Logging;
+
+// Create builder.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add log.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,12 +21,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Index message.
+app.MapGet("/", () => "Hello world, this is Minato version INDEV v1.0.0.");
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapGet("/serverinfo", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -34,8 +40,3 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
